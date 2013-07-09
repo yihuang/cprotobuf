717dcfe269cdc3405df27a7333f67470b4ddc299
huangyi <yi.codeplayer@gmail.com>
2013-07-09 22:51:32 +0800

    fix

--- benchmark/bench.py
+++ benchmark/bench.py
@@ -16,7 +16,7 @@ def encode():
 def decode():
     bs = '\x08\x96\x01\x12\x06\xe6\xb5\x8b\xe8\xaf\x95\x18\xab\x02'
     for i in range(500):
-        decode_object(Test, bs)
+        decode_object(Test(), bs)
 
 if __name__ == '__main__':
     decode()
