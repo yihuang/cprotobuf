e09adc281e54dc3b20343831b4827170a0b61d36
huangyi <yi.codeplayer@gmail.com>
2013-07-18 10:15:01 +0800

    distribute c extension

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
 
-ext_modules = [ Extension("pyprotobuf", ["pyprotobuf.pyx"])
+ext_modules = [ Extension("pyprotobuf", ["pyprotobuf.c"])
               ]
 
 setup(
     name = 'py-protobuf',
-    cmdclass = {'build_ext': build_ext},
     ext_modules = ext_modules,
     scripts = [ 'protoc-gen-cython' ],
 )
