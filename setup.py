f685004b01f6236a302c6450665ff3333b907177
黄毅 <yi.codeplayer@gmail.com>
2014-08-01 11:50:51 +0800

    upload to pypi

--- setup.py
+++ setup.py
@@ -6,10 +6,14 @@ ext_modules = [ Extension("cprotobuf/internal", ["cprotobuf/internal.c"])
               ]
 
 setup(
-    version='0.1',
+    version='0.1.1',
     name = 'cprotobuf',
     ext_modules = ext_modules,
     scripts = [ 'protoc-gen-cprotobuf' ],
     packages = ['cprotobuf'],
+    author='huangyi',
+    author_email='yi.codeplayer@gmail.com',
     url = 'https://github.com/yihuang/cprotobuf',
+    description = 'pythonic and high performance protocol buffer implementation.',
+    long_description=open('README.rst').read(),
 )
