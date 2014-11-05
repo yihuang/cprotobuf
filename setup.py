4fd74b6af74f75d2d239ae25a1532fc6a1ad7d73
黄毅 <yi.codeplayer@gmail.com>
2014-11-05 16:02:14 +0800

    support python-for-android receipe

--- setup.py
+++ setup.py
@@ -2,18 +2,30 @@
 from distutils.core import setup
 from distutils.extension import Extension
 
-ext_modules = [ Extension("cprotobuf/internal", ["cprotobuf/internal.c"])
-              ]
+try:
+    from Cython.Distutils import build_ext
+    have_cython = True
+except ImportError:
+    have_cython = False
+
+if have_cython:
+    ext_modules = [Extension("cprotobuf/internal", ["cprotobuf/internal.pyx"])
+                   ]
+    cmdclass = {'build_ext': build_ext}
+else:
+    cmdclass = {}
+    ext_modules = [Extension("cprotobuf/internal", ["cprotobuf/internal.c"])
+                   ]
 
 setup(
     version='0.1.1',
-    name = 'cprotobuf',
-    ext_modules = ext_modules,
-    scripts = [ 'protoc-gen-cprotobuf' ],
-    packages = ['cprotobuf'],
+    name='cprotobuf',
+    ext_modules=ext_modules,
+    scripts=['protoc-gen-cprotobuf'],
+    packages=['cprotobuf'],
     author='huangyi',
     author_email='yi.codeplayer@gmail.com',
-    url = 'https://github.com/yihuang/cprotobuf',
-    description = 'pythonic and high performance protocol buffer implementation.',
+    url='https://github.com/yihuang/cprotobuf',
+    description='pythonic and high performance protocol buffer implementation.',
     long_description=open('README.rst').read(),
 )
