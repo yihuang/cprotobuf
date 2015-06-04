aa8f539ae084dabf32fa2673ff2efd9696c3024c
黄毅 <yi.codeplayer@gmail.com>
2015-06-04 17:41:17 +0800

    check nonexists field

--- test/test_skip_optional.py
+++ test/test_skip_optional.py
@@ -6,12 +6,14 @@ s = b'\x08\x02\x10\x01\x1a\x07summons'
 class TestMessage(ProtoEntity):
     type = Field('int32', 1)
     count = Field('int32', 2, required=False)
+    nonexists = Field('int32', 4, required=False)
 
 def test_skip():
     req = TestMessage()
     req.ParseFromString(s)
     assert req.type == 2
     assert req.count == 1
+    assert 'nonexists' not in req.__dict__
 
 if __name__ == '__main__':
     test_skip()
