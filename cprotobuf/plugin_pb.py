76bf3fd9082addf0e3a746ca835ddacfd616f7ee
yihuang <yi.codeplayer@gmail.com>
2019-09-23 03:39:28 +0800

    upgrade to protobuf 3.9.2 meta schema

--- cprotobuf/plugin_pb.py
+++ cprotobuf/plugin_pb.py
@@ -1,16 +1,13 @@
 # coding: utf-8
 from cprotobuf import ProtoEntity, Field
 # file: descriptor.proto
-class ExtensionRange(ProtoEntity):
-    start           = Field('int32',	1, required=False)
-    end             = Field('int32',	2, required=False)
-
 class ReservedRange(ProtoEntity):
     start           = Field('int32',	1, required=False)
     end             = Field('int32',	2, required=False)
 
-class OneofDescriptorProto(ProtoEntity):
-    name            = Field('string',	1, required=False)
+class EnumReservedRange(ProtoEntity):
+    start           = Field('int32',	1, required=False)
+    end             = Field('int32',	2, required=False)
 
 class NamePart(ProtoEntity):
     name_part       = Field('string',	1)
@@ -25,6 +22,14 @@ class UninterpretedOption(ProtoEntity):
     string_value    = Field('bytes',	7, required=False)
     aggregate_value = Field('string',	8, required=False)
 
+class ExtensionRangeOptions(ProtoEntity):
+    uninterpreted_option = Field(UninterpretedOption,	999, repeated=True)
+
+class ExtensionRange(ProtoEntity):
+    start           = Field('int32',	1, required=False)
+    end             = Field('int32',	2, required=False)
+    options         = Field(ExtensionRangeOptions,	3, required=False)
+
 class FileOptions(ProtoEntity):
     # enum OptimizeMode
     SPEED=1
@@ -33,18 +38,23 @@ class FileOptions(ProtoEntity):
     java_package    = Field('string',	1, required=False)
     java_outer_classname = Field('string',	8, required=False)
     java_multiple_files = Field('bool',	10, required=False, default=False)
-    java_generate_equals_and_hash = Field('bool',	20, required=False, default=False)
+    java_generate_equals_and_hash = Field('bool',	20, required=False)
     java_string_check_utf8 = Field('bool',	27, required=False, default=False)
     optimize_for    = Field('enum',	9, required=False, default=SPEED)
     go_package      = Field('string',	11, required=False)
     cc_generic_services = Field('bool',	16, required=False, default=False)
     java_generic_services = Field('bool',	17, required=False, default=False)
     py_generic_services = Field('bool',	18, required=False, default=False)
+    php_generic_services = Field('bool',	42, required=False, default=False)
     deprecated      = Field('bool',	23, required=False, default=False)
     cc_enable_arenas = Field('bool',	31, required=False, default=False)
     objc_class_prefix = Field('string',	36, required=False)
     csharp_namespace = Field('string',	37, required=False)
-    javanano_use_deprecated_package = Field('bool',	38, required=False)
+    swift_prefix    = Field('string',	39, required=False)
+    php_class_prefix = Field('string',	40, required=False)
+    php_namespace   = Field('string',	41, required=False)
+    php_metadata_namespace = Field('string',	44, required=False)
+    ruby_package    = Field('string',	45, required=False)
     uninterpreted_option = Field(UninterpretedOption,	999, repeated=True)
 
 class MessageOptions(ProtoEntity):
@@ -106,6 +116,13 @@ class FieldDescriptorProto(ProtoEntity):
     json_name       = Field('string',	10, required=False)
     options         = Field(FieldOptions,	8, required=False)
 
+class OneofOptions(ProtoEntity):
+    uninterpreted_option = Field(UninterpretedOption,	999, repeated=True)
+
+class OneofDescriptorProto(ProtoEntity):
+    name            = Field('string',	1, required=False)
+    options         = Field(OneofOptions,	2, required=False)
+
 class EnumOptions(ProtoEntity):
     allow_alias     = Field('bool',	2, required=False)
     deprecated      = Field('bool',	3, required=False, default=False)
@@ -124,6 +141,8 @@ class EnumDescriptorProto(ProtoEntity):
     name            = Field('string',	1, required=False)
     value           = Field(EnumValueDescriptorProto,	2, repeated=True)
     options         = Field(EnumOptions,	3, required=False)
+    reserved_range  = Field(EnumReservedRange,	4, repeated=True)
+    reserved_name   = Field('string',	5, repeated=True)
 
 class DescriptorProto(ProtoEntity):
     name            = Field('string',	1, required=False)
@@ -142,7 +161,12 @@ class ServiceOptions(ProtoEntity):
     uninterpreted_option = Field(UninterpretedOption,	999, repeated=True)
 
 class MethodOptions(ProtoEntity):
+    # enum IdempotencyLevel
+    IDEMPOTENCY_UNKNOWN=0
+    NO_SIDE_EFFECTS=1
+    IDEMPOTENT=2
     deprecated      = Field('bool',	33, required=False, default=False)
+    idempotency_level = Field('enum',	34, required=False, default=IDEMPOTENCY_UNKNOWN)
     uninterpreted_option = Field(UninterpretedOption,	999, repeated=True)
 
 class MethodDescriptorProto(ProtoEntity):
@@ -195,17 +219,24 @@ class GeneratedCodeInfo(ProtoEntity):
     annotation      = Field(Annotation,	1, repeated=True)
 
 # file: plugin.proto
+class Version(ProtoEntity):
+    major           = Field('int32',	1, required=False)
+    minor           = Field('int32',	2, required=False)
+    patch           = Field('int32',	3, required=False)
+    suffix          = Field('string',	4, required=False)
+
 class CodeGeneratorRequest(ProtoEntity):
     file_to_generate = Field('string',	1, repeated=True)
     parameter       = Field('string',	2, required=False)
     proto_file      = Field(FileDescriptorProto,	15, repeated=True)
+    compiler_version = Field(Version,	3, required=False)
 
-class CodeGeneratorResponse_File(ProtoEntity):
+class File(ProtoEntity):
     name            = Field('string',	1, required=False)
     insertion_point = Field('string',	2, required=False)
     content         = Field('string',	15, required=False)
 
 class CodeGeneratorResponse(ProtoEntity):
     error           = Field('string',	1, required=False)
-    file            = Field(CodeGeneratorResponse_File,	15, repeated=True)
+    file            = Field(File,	15, repeated=True)
 
