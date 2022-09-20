711f2e2f5619ca4308caf9c971355ce62598c765
HuangYi <huang@crypto.com>
2022-09-20 11:09:26 +0800

    fix metaclass for python3

--- cprotobuf/internal.pyx
+++ cprotobuf/internal.pyx
@@ -170,9 +170,7 @@ class MetaProtoEntity(type):
         register_proto(clsname, newcls)
         return newcls
 
-class ProtoEntity(object):
-    __metaclass__ = MetaProtoEntity
-
+class ProtoEntity(object, metaclass=MetaProtoEntity):
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
 
