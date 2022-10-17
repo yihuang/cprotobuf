af5fd8a75acc131d533201922757bc797843990e
HuangYi <huang@crypto.com>
2022-10-17 09:45:02 +0800

    bump to 0.1.11

--- cprotobuf/__init__.py
+++ cprotobuf/__init__.py
@@ -1,3 +1,5 @@
 from .internal import (Field, MetaProtoEntity, ProtoEntity, decode_primitive,
                        encode_data, encode_primitive, get_proto,
                        register_proto)
+
+__version__ = "0.1.11"
--- setup.py
+++ setup.py
@@ -4,29 +4,28 @@ from distutils.extension import Extension
 
 try:
     from Cython.Distutils import build_ext
+
     have_cython = True
 except ImportError:
     have_cython = False
 
 if have_cython:
-    ext_modules = [Extension("cprotobuf.internal", ["cprotobuf/internal.pyx"])
-                   ]
-    cmdclass = {'build_ext': build_ext}
+    ext_modules = [Extension("cprotobuf.internal", ["cprotobuf/internal.pyx"])]
+    cmdclass = {"build_ext": build_ext}
 else:
     cmdclass = {}
-    ext_modules = [Extension("cprotobuf.internal", ["cprotobuf/internal.c"])
-                   ]
+    ext_modules = [Extension("cprotobuf.internal", ["cprotobuf/internal.c"])]
 
 setup(
-    version='0.1.10',
-    name='cprotobuf',
+    version="0.1.11",
+    name="cprotobuf",
     ext_modules=ext_modules,
-    scripts=['protoc-gen-cprotobuf'],
-    packages=['cprotobuf'],
-    author='huangyi',
-    author_email='yi.codeplayer@gmail.com',
-    url='https://github.com/yihuang/cprotobuf',
-    description='pythonic and high performance protocol buffer implementation.',
+    scripts=["protoc-gen-cprotobuf"],
+    packages=["cprotobuf"],
+    author="huangyi",
+    author_email="yi.codeplayer@gmail.com",
+    url="https://github.com/yihuang/cprotobuf",
+    description="pythonic and high performance protocol buffer implementation.",
     cmdclass=cmdclass,
-    long_description=open('README.rst').read(),
+    long_description=open("README.rst").read(),
 )
