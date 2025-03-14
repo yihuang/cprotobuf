023f1cb255ec6bd15492809e46bf9f7b018bc875
mmsqe <mavis@crypto.com>
2025-03-14 12:18:22 +0800

    Problem: cprotobuf is not supporting PEP 517 builds
    add pyproject.toml

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup, Extension
 
 try:
     from Cython.Distutils import build_ext
